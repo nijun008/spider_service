9495659fccf0aa7e114998974ba4cf2a85975e3b
NJ <87179471@qq.com>
2021-01-09 21:34:57 +0800

    简单树洞优化

--- spider/jiandan/index.js
+++ spider/jiandan/index.js
@@ -1,49 +1,6 @@
-const mysql = require('mysql')
-const uuid = require('uuid')
-const dayjs = require('dayjs')
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '123456',
-  port: '3306',
-  database: 'hot_dev'
-})
+const spiderShudong = require('./shudong')
 
-const getShudong = require('./shudong')
-
-function spiderShudong () {
-
-  getShudong(list => {
-    connection.connect()
-
-    list.forEach(item => {
-
-      connection.query('SELECT * FROM `shudong` WHERE `originId` = ?', [item.originId], (err, result) => {
-        if (!result.length) {
-
-          let createDate = dayjs().format('YYYY-MM-DD HH:mm:ss')
-          let row = { 
-            ...item,
-            createDate,
-            updateDate: createDate,
-            id: uuid.v1()
-          }
-          connection.query('INSERT INTO shudong SET ?', row, (err, result) => {
-            if (err) {
-              console.log('插入数据出错', err)
-            } else {
-              console.log('数据插入成功 id：' + row.id)
-            }
-          })
-
-        }
-      })
-    })
-
-    // connection.end()
-  })
-}
 
 module.exports = {
   spiderShudong
--- spider/jiandan/shudong.js
+++ spider/jiandan/shudong.js
@@ -1,12 +1,20 @@
+// 树洞
+
 const Nightmare = require('nightmare')
 const cheerio = require("cheerio")
 const host = require('./config').host
 
+const mysql = require('mysql')
+const uuid = require('uuid')
+const dayjs = require('dayjs')
+
+
 const nightmare = Nightmare()
 
-function getShudong (cb) {
 
-  nightmare
+function getShudong () {
+
+  return nightmare
   .goto(`${host}top-comments`)
   .evaluate(() => {
     return document.querySelector('#comment').innerHTML
@@ -31,7 +39,6 @@ function getShudong (cb) {
       comments.push(comment)
     })
 
-    cb && cb(comments)
     return comments
   })
   .catch(() => {
@@ -40,4 +47,54 @@ function getShudong (cb) {
   
 }
 
-module.exports = getShudong
+async function spiderShudong () {
+  
+  let list = await getShudong()
+  console.log(list)
+
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'queen',
+    port: '3306',
+    database: 'hot_dev'
+  })
+
+  connection.connect()
+  console.log('数据库连接')
+
+  
+  list.forEach(item => {
+    connection.query('SELECT * FROM `shudong` WHERE `originId` = ?', [item.originId], (err, result) => {
+      if (!result.length) {
+
+        let createDate = dayjs().format('YYYY-MM-DD HH:mm:ss')
+        let row = { 
+          ...item,
+          createDate,
+          updateDate: createDate,
+          id: uuid.v1()
+        }
+        connection.query('INSERT INTO shudong SET ?', row, (err, result) => {
+          if (err) {
+            console.log('插入数据出错', err)
+          } else {
+            console.log(`数据插入成功 id ${row.id}`)
+          }
+        })
+
+      } else {
+        console.log(`数据已存在 originId: ${item.originId}`)
+      }
+    })
+
+  })
+
+  setTimeout(() => {
+    connection.end()
+    console.log('数据库断开')
+  }, 10)
+  
+}
+
+module.exports = spiderShudong
